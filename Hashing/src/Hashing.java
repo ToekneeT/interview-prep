719ddd5b6797ace02d5af9de69535cc939a06c4b
TT <tony.trinh58@gmail.com>
2024-03-24 17:44:54 -0700

    Hash map created.
    Implemented a hash map in Java. Included test code as well.

---
+++ Hashing/src/Hashing.java
@@ -0,0 +1,143 @@
+public class Hashing<K, V> {
+
+    private class Pair<K, V> {
+        private K key;
+        private V value;
+        private Pair<K, V> next;
+
+        public Pair(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return this.key;
+        }
+
+        public V getValue() {
+            return this.value;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
+    }
+
+    public int size = 7;
+    private Pair<K, V> map[];
+    private Pair<K, V> newMap[];
+
+    public Hashing() {
+        map = new Pair[size];
+    }
+
+//    private Hashing(int s) {
+//        newMap = new Pair[s];
+//    }
+
+    public void set(K key, V value) {
+        int hash = key.hashCode() % size;
+        Pair<K, V> pair = map[hash];
+
+        if (pair == null) {
+            map[hash] = new Pair<K, V>(key, value);
+        }
+        else {
+            while (pair.next != null) {
+                if (pair.getKey() == key) {
+                    pair.setValue(value);
+                    return;
+                }
+                pair = pair.next;
+            }
+            if (pair.getKey() == key) {
+                pair.setValue(value);
+                return;
+            }
+            pair.next = new Pair<K, V>(key, value);
+        }
+    }
+
+    public V get(K key) {
+        int hash = key.hashCode() % size;
+        Pair<K, V> pair = map[hash];
+
+        if (pair == null) {
+            return null;
+        }
+        while (pair != null) {
+            if (pair.getKey() == key) {
+                return pair.getValue();
+            }
+            pair = pair.next;
+        }
+        return null;
+    }
+
+    public void del(K key) {
+        int hash = key.hashCode() % size;
+        Pair<K, V> pair = map[hash];
+
+        if (pair.getKey() == key) {
+            map[hash] = pair.next;
+            pair.next = null;
+        }
+        Pair<K, V> prev = pair;
+        while (pair != null) {
+            if (pair.getKey() == key) {
+                prev.next = pair.next;
+                pair.next = null;
+            }
+            prev = pair;
+            pair = pair.next;
+        }
+    }
+
+    public boolean in(K key) {
+        int hash = key.hashCode() % size;
+        Pair<K, V> pair = map[hash];
+
+        if (pair == null) {
+            return false;
+        }
+        else if (pair.getKey() != key) {
+            return false;
+        }
+        return true;
+    }
+
+    public int sizeOf() {
+        int mapSize = 0;
+        for (int i = 0; i < size; i++) {
+            Pair<K, V> nextNode = map[i];
+            while (nextNode != null) {
+                nextNode = nextNode.next;
+                mapSize++;
+            }
+        }
+        return mapSize;
+    }
+
+    public double load() {
+        double load = 0;
+        for (int i = 0; i < size; i++) {
+            if (map[i] != null) {
+                load++;
+            }
+        }
+        return load / size;
+    }
+
+    public void resize(int newSize) {
+        newMap = new Pair[newSize];
+        int hash;
+        for (int i = 0; i < size; i++) {
+            if (map[i] != null) {
+                hash = map[i].getKey().hashCode() % newSize;
+                newMap[hash] = map[i];
+            }
+        }
+        map = newMap;
+        size = newSize;
+    }
+}
---
+++ Hashing/src/HashingTest.java
@@ -0,0 +1,78 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+public class HashingTest {
+    @Test
+    void testGet() {
+        Hashing<String, String> map = new Hashing<String, String>();
+        map.set("Foo", "Bar");
+        assertEquals("Bar", map.get("Foo"));
+    }
+
+    @Test
+    void testDel() {
+        Hashing<String, String> map = new Hashing<String, String>();
+        map.set("Foo", "Bar");
+        map.set("Bar", "Foo");
+        assertEquals("Bar", map.get("Foo"));
+        map.del("Foo");
+        assertEquals(null, map.get("Foo"));
+        assertEquals("Foo", map.get("Bar"));
+    }
+
+    @Test
+    void testSizeOf() {
+        Hashing<String, String> map = new Hashing<String, String>();
+        assertEquals(0, map.sizeOf());
+        map.set("Foo", "Bar");
+        assertEquals(1, map.sizeOf());
+        map.set("Bar", "Foo");
+        assertEquals(2, map.sizeOf());
+        map.set("A", "B");
+        map.set("C", "D");
+        map.set("E", "F");
+        map.set("G", "H");
+        assertEquals(6, map.sizeOf());
+        map.set("I", "J");
+        map.set("K", "L");
+        map.set("M", "N");
+        map.set("O", "P");
+        assertEquals(10, map.sizeOf());
+    }
+
+    @Test
+    void testIn() {
+        Hashing<String, String> map = new Hashing<String, String>();
+        map.set("Foo", "Bar");
+        assertEquals(true, map.in("Foo"));
+        assertEquals(false, map.in("Bar"));
+    }
+
+    @Test
+    void testLoad() {
+        Hashing<String, String> map = new Hashing<String, String>();
+        map.set("Foo", "Bar");
+        assertEquals(1.0 / 7, map.load());
+        map.set("Bar", "Foo");
+        assertEquals(2.0 / 7, map.load());
+        map.set("A", "B");
+        assertEquals(3.0 / 7, map.load());
+        map.set("C", "D");
+        assertEquals(4.0 / 7, map.load());
+        map.set("E", "F");
+        assertEquals(5.0 / 7, map.load());
+        map.set("G", "H");
+        assertEquals(6.0 / 7, map.load());
+        map.set("M", "N");
+        assertEquals(7.0 / 7, map.load());
+    }
+
+    @Test
+    void testResize() {
+        Hashing<String, String> map = new Hashing<String, String>();
+        map.set("Foo", "Bar");
+        assertEquals(1.0 / 7, map.load());
+        map.resize(5);
+        assertEquals(1.0 / 5, map.load());
+    }
+}
