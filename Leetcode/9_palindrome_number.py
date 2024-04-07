77086a9dbd7b9c0193b0ceaca2fd161180bb35bb
TT <tony.trinh58@gmail.com>
2024-04-06 22:55:18 -0700

    added a third way to do palindrome.

--- Leetcode/9_palindrome_number.py
+++ Leetcode/9_palindrome_number.py
@@ -18,6 +18,7 @@ def isPalindrome(x):
         right = x[len(x) // 2 + 1:]
     return left == right[::-1]
 '''
+'''
 # Done without turning into a string
 def isPalindrome(x):
     if x < 0:
@@ -37,4 +38,20 @@ def isPalindrome(x):
 
 # After thinking of this solution, I realized I could've just reversed the
 # entire string on the first solution instead of splitting it in half
-# like this one is doing.
+'''
+# After our recursion lesson, I was thinking about some functions I could do for recursion.
+# Realized I used a while loop for this, so thought it'd be possible to do.
+
+# reverse should be 0 for a reverse.
+# used to track the reversed number within the 
+# recursion
+def reverse_num(num: int, reverse: int) -> int:
+    if num == 0:
+        return reverse
+    ones = num % 10
+    return reverse_num(num // 10, reverse * 10 + ones)
+
+def isPalindome(x):
+    if x < 0:
+        return False
+    return x == reverse_num(x, 0)
